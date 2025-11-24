f6759e498406c4181c85216541ca5ef83e39fe70
Betim <sherifibetim@gmail.com>
2025-11-24 18:56:58 +0100

    adds standalone board component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskBoard } from './kanban/task-board/task-board';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskBoard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
